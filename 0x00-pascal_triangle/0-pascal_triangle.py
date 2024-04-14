8c3703c549c13049a1e6e4be88fd4866a9053c2c
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2024-04-14 15:25:52 +0300

    pascal math in python

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,22 @@
+def pascal_triangle(n):
+    """a function that returns a list of lists of
+    integers representing the Pascal’s triangle of n.
+    
+
+    Args:
+        n (int): number of rows.
+    """
+    outerlist = []
+    if n <= 0:
+        return outerlist
+    
+    for row in range(n):
+        innerlist = []
+        for column in range(row + 1):
+            if column == 0 or column == row:
+                innerlist.append(1)
+            else:
+                innerlist.append(outerlist[row-1][column-1] + outerlist[row-1][column])
+        outerlist.append(innerlist)
+    
+    return outerlist
---
+++ 0x00-pascal_triangle/README.md
@@ -0,0 +1,2 @@
+<h1>Alx interview project one</h1>
+<h3>It is on pascal triangle </h3>
