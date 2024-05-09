28d1877943ab7ffabd41b0a5f4cb699389702b53
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2024-05-09 22:52:27 +0300

    UTF-8 Validation task

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -2,50 +2,41 @@
 """
 UTF-8 Validation
 """
-from itertools import takewhile
-
-
-def int_to_bits(nums):
-    """
-    Helper function
-    Convert ints to bits
-    """
-    for num in nums:
-        bits = []
-        mask = 1 << 8  # cause we have 8 bits per byte. adds up to (11111111)
-        while mask:
-            mask >>= 1
-            bits.append(bool(num & mask))
-        yield bits
 
 
 def validUTF8(data):
-    """
-    Takes a list of ints and returns true if the list is
-    a valid UTF-8 encoding, else returns false
-    Args:
-        data : List of ints representing possible UTF-8 encoding
-    Return:
-        bool : True or False
-    """
-    bits = int_to_bits(data)
-    for byte in bits:
-        # if single byte char, then valid. continue
-        if byte[0] == 0:
+    def is_continuation(byte):
+        return (byte & 0b11000000) == 0b10000000
+
+    if not data:
+        return True
+
+    i = 0
+    while i < len(data):
+        byte = data[i]
+
+        # Single-byte character (ASCII)
+        if byte < 128:
+            i += 1
             continue
 
-        # if here, byte is multi-byte char
-        ones = sum(takewhile(bool, byte))
-        if ones <= 1:
+        # Determine the number of bytes for multi-byte characters
+        if byte < 192:
             return False
-        if ones >= 4:  # UTF-8 can be 1 to 4 bytes long
+        elif byte < 224:
+            num_bytes = 2
+        elif byte < 240:
+            num_bytes = 3
+        elif byte < 248:
+            num_bytes = 4
+        else:
             return False
 
-        for _ in range(ones - 1):
-            try:
-                byte = next(bits)
-            except StopIteration:
+        # Check continuation bytes
+        i += 1
+        for _ in range(num_bytes - 1):
+            if i >= len(data) or not is_continuation(data[i]):
                 return False
-            if byte[0:2] != [1, 0]:
-                return False
+            i += 1
+
     return True
