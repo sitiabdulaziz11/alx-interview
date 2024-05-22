611a8e2ed3353a61748031180640a4c3098da00d
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2024-05-22 11:10:42 +0300

    js request

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,48 +2,16 @@
 
 const request = require('request');
 
-const movieId = process.argv[2];
-
-if (!movieId) {
-  console.error('Please provide a movie ID.');
-  process.exit(1);
-}
-
-const url = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
-
-request(url, (error, response, body) => {
-  if (error) {
-    console.error('Error making request:', error);
-    return;
-  }
-
-  if (response.statusCode !== 200) {
-    console.error('Failed to fetch data:', response.statusCode);
-    return;
-  }
-
-  // Parse the response body as JSON
-  const movieData = JSON.parse(body);
-
-  // Extract the list of character URLs from the movie data
-  const characterUrls = movieData.characters;
-
-  // Loop through each character URL and make a request to get the character name
-  characterUrls.forEach((characterUrl) => {
-    request(characterUrl, (charError, charResponse, charBody) => {
-      if (charError) {
-        console.error('Error fetching character:', charError);
-        return;
-      }
-
-      if (charResponse.statusCode !== 200) {
-        console.error('Failed to fetch character data:', charResponse.statusCode);
-        return;
-      }
-
-      // Parse the character data and print the character name
-      const characterData = JSON.parse(charBody);
-      console.log(characterData.name);
-    });
-  });
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
+  if (err) throw err;
+  const actors = JSON.parse(body).characters;
+  exactOrder(actors, 0);
 });
+const exactOrder = (actors, x) => {
+  if (x === actors.length) return;
+  request(actors[x], function (err, res, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    exactOrder(actors, x + 1);
+  });
+};
